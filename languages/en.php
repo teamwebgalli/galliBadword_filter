232a6fa3c6925f45919e7ba882f80bd8712e0142
Team Webgalli <webgalli@gmail.com>
2015-09-05 22:25:22 +0530

    Revert "Updated for Elgg 1.1X"
    This reverts commit 23df328b17f93fba1b18f5f3723d557c1294f965.

--- languages/en.php
+++ languages/en.php
@@ -11,6 +11,8 @@
  *	Licence : GNU2
  *	Copyright : Team Webgalli 2011-2015
  */
-return array(
+$english = array(
 	'gBwf:addmorewords' => 'Add more badwords as comma seperated. If you have a lot of badwords, please add it directly to /mod/galliBadword_filter/lib/badwords.php',
 );
+
+add_translation("en", $english);
